4f7581d403278dacedcf3236dac75de3797d8f8f
John Hinz <jhinz402@hotmail.com>
2022-05-28 12:36:02 -0400

    Add project files.

---
+++ Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Azure.Messaging.ServiceBus;
+
+public class Program
+{
+
+    static string connectionString = "Endpoint=sb://learningazuremsgbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=kBtAbYrmlQoBAWzeADEA9mkbLo4G5FDco8Ng2pX8nHE=";
+
+    static string topicName = "humberchat";
+
+    static ServiceBusClient client;
+
+    static ServiceBusSender sender;
+
+    static async Task Main()
+    {
+        client = new ServiceBusClient(connectionString);
+        sender = client.CreateSender(topicName);
+
+        //using ServiceBusMessage message = await sender.CreateMessageAsync();
+
+        while (true)
+        {
+            Console.WriteLine("Enter a message");
+            string input = Console.ReadLine();
+            if (input == "exit")
+            {
+                break;
+            }
+
+            ServiceBusMessage message = 
+                new ServiceBusMessage($"John says:{input}");
+
+            await sender.SendMessageAsync(message);
+
+        }
+
+        await sender.DisposeAsync();
+        await client.DisposeAsync();
+
+        Console.WriteLine("Messaging complete");
+    }
+
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using Azure.Messaging.ServiceBus;
+
+class Program
+{    
+    static string  connectionString = "Endpoint=sb://learningazuremsgbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=";
+
+    static string topicName = "humberchat";
+
+    static string subscriptionName = "HumberChatSubPup";
+
+    static ServiceBusClient client;
+
+    static ServiceBusProcessor processor;
+
+    static async Task MessageHandler(ProcessMessageEventArgs arg)
+    {
+        Console.WriteLine($"Received: {arg.Message.Body.ToString()}");
+        await arg.CompleteMessageAsync(arg.Message);
+    }
+
+    static Task ErrorHandler(ProcessErrorEventArgs arg)
+    {
+        Console.WriteLine(arg.Exception.ToString());
+        return Task.CompletedTask;
+    }
+
+    static async Task Main()
+    {
+        client = new ServiceBusClient(connectionString);
+        processor =
+            client.CreateProcessor(
+                topicName, subscriptionName);
+
+        processor.ProcessMessageAsync += MessageHandler;
+        processor.ProcessErrorAsync += ErrorHandler;
+
+        await processor.StartProcessingAsync(); 
+
+        Console.WriteLine("Listneing for messages");
+        Console.ReadLine(); 
+
+        await processor.StopProcessingAsync();
+
+        await processor.DisposeAsync();
+        await client.DisposeAsync();
+
+
+    }
+}
